c996402e0ce534b03d64f4351bad2acdae455031
ShayanAbedi <abedi.shayan@unb.ca>
2020-02-29 15:30:39 -0400

    fixed indention

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from flask import Flask, jsonify, abort, request, make_response
 from flask_restful import Resource, Api
@@ -10,7 +11,7 @@ import cgi
 import sys
 cgitb.enable()
 
-import settings # Our server and db settings, stored in settings.py
+import settings  # Our server and db settings, stored in settings.py
 
 app = Flask(__name__, static_url_path='/static')
 api = Api(app)
@@ -20,54 +21,69 @@ api = Api(app)
 #
 # Error handlers
 #
-@app.errorhandler(400) # decorators to add to 400 response
-def not_found(error):
-	return make_response(jsonify( { "status": "Bad request" } ), 400)
+                       # decorators to add to 400 response
 
-@app.errorhandler(404) # decorators to add to 404 response
+@app.errorhandler(400)
 def not_found(error):
-	return make_response(jsonify( { "status": "Resource not found" } ), 404)
+    return make_response(jsonify({'status': 'Bad request'}), 400)
+
+
+                       # decorators to add to 404 response
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'status': 'Resource not found'}), 404)
+
 
 ####################################################################################
 #
 # Static Endpoints for humans
 #
-class Root(Resource):
-	def get(self):
-		return make_response(jsonify({"message": "Welcome to the GiftRegistrys"}))
 
-api.add_resource(Root,'/')
+class Root(Resource):
+
+    def get(self):
+        return make_response(jsonify({'message': 'Welcome to the GiftRegistrys'
+                             }))
+
+
+api.add_resource(Root, '/')
+
 
 ####################################################################################
 #
 # Users routing: GET and POST, individual school access
 #
-class Users(Resource):
-    # GET: Return all user resources
-	#
-	# Example request: curl http://info3103.cs.unb.ca:xxxxx/users
-	def get(self):
-		try:
-			dbConnection = pymysql.connect(
-				settings.DB_HOST,
-				settings.DB_USER,
-				settings.DB_PASSWD,
-				settings.DB_DATABASE,
-				charset='utf8mb4',
-				cursorclass= pymysql.cursors.DictCursor)
-			sql = 'getUsers'
-			cursor = dbConnection.cursor()
-			cursor.callproc(sql) # stored procedure, no arguments
-			rows = cursor.fetchall() # get all the results
-		except:
-			abort(500) # Nondescript server error
-		finally:
-			cursor.close()
-			dbConnection.close()
-		return make_response(jsonify({'users': rows}), 200) # turn set into json and return it
-    
 
-	def post(self):
+class Users(Resource):
+
+    # GET: Return all user resources
+    #
+    # Example request: curl http://info3103.cs.unb.ca:xxxxx/users
+
+    def get(self):
+        try:
+            dbConnection = pymysql.connect(
+                settings.DB_HOST,
+                settings.DB_USER,
+                settings.DB_PASSWD,
+                settings.DB_DATABASE,
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor,
+                )
+            sql = 'getUsers'
+            cursor = dbConnection.cursor()
+            cursor.callproc(sql)  # stored procedure, no arguments
+            rows = cursor.fetchall()  # get all the results
+        except:
+            abort(500)  # Nondescript server error
+        finally:
+            cursor.close()
+            dbConnection.close()
+        return make_response(jsonify({'users': rows}), 200)  # turn set into json and return it
+
+    def post(self):
+
         #
         # Sample command line usage:
         #
@@ -75,58 +91,66 @@ class Users(Resource):
         #    -d '{"email": "test@gmail.com", "user_name": "test Adding Users""}'
         #         http://info3103.cs.unb.ca:41921/users
 
-		if not request.json or not 'user_name' in request.json:
-			abort(400) # bad request
+        if not request.json or not 'user_name' in request.json:
+            abort(400)  # bad request
 
-		# The request object holds the ... wait for it ... client request!
-		# Pull the results out of the json request
-		userName = request.json['user_name'];
-		email = request.json['email'];
-    
-        	if ('img_url' in request.json):
-            		img = request.json['img_url'];
-        	else:
-            		img = NULL
-            
-		
+        # The request object holds the ... wait for it ... client request!
+        # Pull the results out of the json request
 
-       
+        userName = request.json['user_name']
+        email = request.json['email']
 
-		try:
-			dbConnection = pymysql.connect(settings.DB_HOST,
-				settings.DB_USER,
-				settings.DB_PASSWD,
-				settings.DB_DATABASE,
-				charset='utf8mb4',
-				cursorclass= pymysql.cursors.DictCursor)
-			sql = 'addUser'
-			cursor = dbConnection.cursor()
-			sqlArgs = (userName, email, img) # Must be a collection
-			cursor.callproc(sql,sqlArgs) # stored procedure, with arguments
-			row = cursor.fetchone()
-			dbConnection.commit() # database was modified, commit the changes
-		except:
-			abort(500) # Nondescript server error
-		finally:
-			cursor.close()
-			dbConnection.close()
-		# Look closely, Grasshopper: we just created a new resource, so we're
-		# returning the uri to it, based on the return value from the stored procedure.
-		# Yes, now would be a good time check out the procedure.
-		# uri = 'http://'+settings.APP_HOST+':'+str(settings.APP_PORT)
-		# uri = uri+str(request.url_rule)+'/'+str(row['LAST_INSERT_ID()'])
-		return make_response(jsonify( { "message" : "success!" } ), 201) # successful resource creation
+    if 'img_url' in request.json:
+        img = request.json['img_url']
+    else:
+        img = NULL
+
+        try:
+            dbConnection = pymysql.connect(
+                settings.DB_HOST,
+                settings.DB_USER,
+                settings.DB_PASSWD,
+                settings.DB_DATABASE,
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor,
+                )
+            sql = 'addUser'
+            cursor = dbConnection.cursor()
+            sqlArgs = (userName, email, img)  # Must be a collection
+            cursor.callproc(sql, sqlArgs)  # stored procedure, with arguments
+            row = cursor.fetchone()
+            dbConnection.commit()  # database was modified, commit the changes
+        except:
+            abort(500)  # Nondescript server error
+        finally:
+            cursor.close()
+            dbConnection.close()
+
+        # Look closely, Grasshopper: we just created a new resource, so we're
+        # returning the uri to it, based on the return value from the stored procedure.
+        # Yes, now would be a good time check out the procedure.
+        # uri = 'http://'+settings.APP_HOST+':'+str(settings.APP_PORT)
+        # uri = uri+str(request.url_rule)+'/'+str(row['LAST_INSERT_ID()'])
+
+        return make_response(jsonify({'message': 'success!'}), 201)  # successful resource creation
+
+
 ####################################################################################
 #
 # Identify/create endpoints and endpoint objects
 #
+
 api = Api(app)
 api.add_resource(Users, '/users')
 
-
-
 #############################################################################
 # xxxxx= last 5 digits of your studentid. If xxxxx > 65535, subtract 30000
-if __name__ == "__main__":
+
+if __name__ == '__main__':
+
 #    app.run(host="info3103.cs.unb.ca", port=xxxx, debug=True)
-	app.run(host=settings.APP_HOST, port=settings.APP_PORT, debug=settings.APP_DEBUG)
+
+    app.run(host=settings.APP_HOST, port=settings.APP_PORT,
+            debug=settings.APP_DEBUG)
+
+			
