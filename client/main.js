fa0bf0f1e8a81dc44e6733c10c2aa862ec8ac98b
ShayanAbedi <abedi.shayan@unb.ca>
2020-03-30 20:35:00 -0300

    started the frontend - created a landing page and show presents in bootstrap cards

--- client/main.js
+++ client/main.js
@@ -1,21 +1,66 @@
-// register modal component
-Vue.component('modal', {
-  template: '#modal-template'
-});
-
 var app = new Vue({
   el: '#app',
 
   //------- data --------
   data: {
-    serviceURL: 'https://info3103.cs.unb.ca:8038',
+    serviceURL: 'http://info3103.cs.unb.ca:8038',
+    mainPage: true,
     authenticated: false,
-    schoolsData: null,
     loggedIn: null,
-    editModal: false,
+    presentsData: null,
     input: {
       username: '',
       password: ''
     }
+  },
+  methods: {
+    login() {
+      if (this.input.username != '' && this.input.password != '') {
+        axios
+          .post(this.serviceURL + '/signin', {
+            username: this.input.username,
+            password: this.input.password
+          })
+          .then(response => {
+            if (response.data.status == 'success') {
+              this.authenticated = true;
+              this.loggedIn = response.data.username;
+            }
+            console.log(this.loggedIn);
+          })
+          .catch(e => {
+            alert('The username or password was incorrect, try again');
+            this.input.password = '';
+            console.log(e);
+          });
+      } else {
+        alert('A username and password must be present');
+      }
+    },
+
+    logout() {
+      axios
+        .delete(this.serviceURL + '/signin')
+        .then(response => {
+          location.reload();
+          alert('You logged out successfully!');
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+
+    getPresents() {
+      axios
+        .get(this.serviceURL + '/presents')
+        .then(response => {
+          this.presentsData = response.data.presents;
+        })
+        .catch(e => {
+          alert('Unable to retrieve the presents back');
+          console.log(e);
+        });
+    }
   }
+  //------- END methods --------
 });
